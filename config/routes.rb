9a775b375722f684081babd4a695d1b7797e0450
takasugiren <bucyou16528080@gmail.com>
2021-05-12 08:49:33 +0000

    [Update]1

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
-  get 'users/update'
   devise_for :users
+  root to: 'homes#top'
+  resources :books, only: [:new, :index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show, :edit, :update]
 end
