c091edd4d4e960892c1e3eb552187524b0c011f4
takasugiren <bucyou16528080@gmail.com>
2021-05-14 08:23:24 +0000

    [Update]4

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,26 +1,26 @@
 class BooksController < ApplicationController
     def index
       @book = Book.new
-      
+      @books = Book.all
     end
-    
+
     def show
       @book = Book.new
       @name = user.name
     end
-    
+
     def new
       @book = Book.new
     end
-    
+
     def create
-      book = Book.new
-      book.save
-      redirect_to 'user_path'
+      @book = Book.new(book_params)
+      @book.save
+      redirect_to books_path
     end
-    
+
     private
     def book_params
-      params.repuire(:book).permit(:title, :body, :image)
+      params.require(:book).permit(:title, :body, :image)
     end
 end
